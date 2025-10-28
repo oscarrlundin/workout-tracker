da62266f3d690efdad4f68a7a3c230fe72b80c48
Oscar Lundin <oscarrlundin@gmail.com>
2025-10-28 07:56:32 +0100

    icons: wire bottom nav icons in Icon map

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,14 +1,24 @@
 // src/components/Icon.jsx
-import CalendarIcon from '@/icons/calendar.svg?react'
-import TemplatesIcon from '@/icons/templates.svg?react'
-import ChevronDownIcon from '@/icons/chevron-down.svg?react'
-import PlusIcon from '@/icons/plus.svg?react'
+import CalendarIcon     from '@/icons/calendar.svg?react'
+import TemplatesIcon    from '@/icons/templates.svg?react'
+import ChevronDownIcon  from '@/icons/chevron-down.svg?react'
+import PlusIcon         from '@/icons/plus.svg?react'
+import HomeIcon       from '@/icons/home.svg?react'
+import TrendingUpIcon from '@/icons/trending-up.svg?react'
+import DumbbellIcon   from '@/icons/dumbbell.svg?react'
+import GridIcon       from '@/icons/grid.svg?react'
+import SettingsIcon   from '@/icons/settings.svg?react'
 
 const map = {
   calendar: CalendarIcon,
   templates: TemplatesIcon,
   'chevron-down': ChevronDownIcon,
   plus: PlusIcon,
+  home: HomeIcon,
+  progress: TrendingUpIcon,
+  exercises: DumbbellIcon,
+  grid: GridIcon,
+  settings: SettingsIcon,
 }
 
 export default function Icon({ name, className = '', ariaLabel }) {
